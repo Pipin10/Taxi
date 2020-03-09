566360e64ec16342a20a24ad199ee4df6ee3fde7
Pipin <pipin.ristriani@students.amikom.ac.id>
2020-03-09 16:58:43 +0700

    melengkapi file

--- calculator.cpp
+++ calculator.cpp
@@ -2,6 +2,16 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
-		
+		int a= 10;
+		int b= 6;
+	}
+	static int penambahan(int a, int b)
+	{
+		return a + b;
+	}
+	
+	static int pengurangan(int a, int b)
+	{
+		return a - b;
 	}
 }
